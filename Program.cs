46bb71433577342ae549a92dae379e77361d2da5
mehul9595 <mehul9595@gmail.com>
2020-09-22 19:17:32 +0800

    add basic project configuration template

--- Program.cs
+++ Program.cs
@@ -1,11 +1,23 @@
 ﻿using System;
+using Microsoft.Extensions.Configuration;
+using Azure.Storage;
+using Azure.Storage.Blobs;
+using System.Threading.Tasks;
+using System.IO;
 
 namespace BlobManagerApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            string path = Path.GetFullPath(@"appsettings.json");
+            Console.WriteLine($"{path}");
+            IConfiguration configuration = new ConfigurationBuilder()
+                                                .AddJsonFile(path).Build();
+
+            Console.Write($"{configuration["storageAccountName"]}");
+
             Console.WriteLine("Hello World!");
         }
     }
